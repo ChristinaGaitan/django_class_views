b6fc70cfdd718a2ebac348cff05ce23d0dfd6994
ChristinaGaitan <christina.gaitan@gmail.com>
2021-02-10 09:06:19 -0600

    Use TemplateView instead of View

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import View
-from django.http import HttpResponse
+from django.views.generic import View, TemplateView
 
-class VBView(View):
-  def get(self, request):
-    return HttpResponse('CLASE BASE VIEWS ARE COOL!')
+class IndexView(TemplateView):
+  template_name = 'index.html'
